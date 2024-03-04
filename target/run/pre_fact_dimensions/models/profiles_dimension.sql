2b88afab9ed5d5d34d184751571fdbe12223d94b
frefaat <fagrahmed@gmail.com>
2024-03-04 21:04:33 +0200

    profiles_dimension

---
+++ target/run/pre_fact_dimensions/models/profiles_dimension.sql
@@ -0,0 +1,39 @@
+
+  
+    
+
+  create  table "dbt"."dbt-dimensions"."profiles_dimension__dbt_tmp"
+  
+  
+    as
+  
+  (
+    -- models/profiles_dimension.sql
+
+
+
+SELECT
+    wp.walletprofileid,
+    wp.partnerid,
+
+    (wp.createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as profile_createdat_utc2,
+    (wp.updatedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as profile_modifiedat_utc2,
+    (wp.deletedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as profile_deletedat_utc2,
+
+    wp.type as profile_type,
+    p.partnernameen as partner_name,
+    activeflag as partner_isactive,
+
+    (p.createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as partner_createdat_utc2,
+    (p.lastmodifiedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as partner_modifiedat_utc2,
+    (p.deletedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as partner_deletedat_utc2,
+
+
+    (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as loaddate,
+    null::timestamptz as expdate,
+    true::boolean as currentflag
+
+FROM "dbt"."axis_core"."walletprofiles" wp
+LEFT JOIN "dbt"."axis_kyc"."partner" p on wp.partnerid = p.partnerid
+  );
+  
